896f94a4ccbaaf5b520abe59ab30f30f71558ffc
ap35 <ap35@rice.edu>
2019-06-27 13:50:15 -0400

    added form for text

---
+++ my-app/src/Form.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Form.css'
+
+class Form extends React.Component {
+    validateText = function(e){
+        var text = document.getElementById('noter-text-area')
+        var sentence = text.value;
+        if (sentence.length >= 10){
+            e.preventDefault();
+            alert('over text limit');
+        }
+    }
+    formSubmit = function(e){
+        alert('it works!');
+        e.preventDefault();
+    }
+    render() {
+        
+        return (
+            <div className="Form">
+            <form id="noter-save-form" method="POST" onSubmit={this.formSubmit}>
+                <textarea id="noter-text-area" name="textarea" value={this.props.name} onKeyPress={this.validateText}></textarea>
+                <br></br>
+                <input className="butn" type="submit" value="Calculate" />
+            </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
