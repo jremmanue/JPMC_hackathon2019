a500e05984bfcfe60907a5ae9e10e09b1bc0fc69
ap35 <ap35@rice.edu>
2019-06-27 15:14:15 -0400

    fixed bug in master

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Overlay></Overlay>
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <nav id="navbar" className="navbar navbar-expand-lg navbar-dark fixed-top">
           <img alt='logo' src={logotest} className="logoStyle" width='90' margintop='-100'/>
           <h2 className='header'>KNOW YOUR DNI</h2>
       </nav>
