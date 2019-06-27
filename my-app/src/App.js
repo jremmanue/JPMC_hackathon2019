651160045a6e735637c148a70198815a37e2d9c4
Juliana Emmanuelli <jremmanue@gmail.com>
2019-06-27 12:30:08 -0400

    playing w charts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,61 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import GaugeChart from 'react-gauge-chart'
 import logo from './logo.svg';
 import './App.css';
+import Result from './result-chart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <form>
-        <input type="textarea" name="name" />
-        <input type="submit" value="Submit" />
-      </form>
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        
+        <Result></Result>
 
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//       <Container>
+//         <Row>
+//           <Col xs={12} lg={{offset: 2, span: 8}}>
+//             <h1>React Gauge Chart Demo</h1>
+//           </Col>
+//         </Row>  
+//         <Row>
+//           <Col xs={12} lg={6}>
+//             <h6>GaugeChart with default props</h6>
+//             <GaugeChart id="gauge-chart1" />
+//           </Col>
+//           <Col xs={12} lg={6}>
+//             <h6>GaugeChart with 20 levels</h6>
+//             <GaugeChart id="gauge-chart2" nrOfLevels={20} percent={0.86} needleColor="#345243" />
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col xs={12} lg={6}>
+//             <h6>GaugeChart with custom colors</h6>
+//             <GaugeChart id="gauge-chart3" nrOfLevels={30} colors={["#FF5F6D", "#FFC371"]} arcWidth={0.3} percent={0.37} />
+//           </Col>
+//           <Col xs={12} lg={6}>
+//             <h6>GaugeChart with larger padding between elements</h6>
+//             <GaugeChart id="gauge-chart4" nrOfLevels={10} arcPadding={0.1} cornerRadius={3} percent={0.6} />
+//           </Col>
+//         </Row>
+//       </Container>
+
+//       </>
+//     );
+//   }
+// }
+
+
 
 export default App;
---
+++ my-app/src/result-chart.js
@@ -0,0 +1,28 @@
+import GaugeChart from 'react-gauge-chart'
+import React from 'react'
+
+class Result extends React.Component {
+    render(){
+        return (
+        <div>
+            <div className="row">
+            <GaugeChart id="gauge-chart1" nrOfLevels={30} colors={["#FF5F6D", "#FFC371"]} arcWidth={0.3} percent={0.37} />
+            </div>
+            <div className="row">
+            <GaugeChart id="gauge-chart2" nrOfLevels={30} colors={["#FF5F6D", "#FFC371"]} arcWidth={0.3} percent={0.37} />
+            </div>
+            <div className="row">
+            <GaugeChart id="gauge-chart3" nrOfLevels={30} colors={["#FF5F6D", "#FFC371"]} arcWidth={0.3} percent={0.37} />
+            </div>
+            <div className="row">
+            <GaugeChart id="gauge-chart4" nrOfLevels={30} colors={["#FF5F6D", "#FFC371"]} arcWidth={0.3} percent={0.37} />
+            </div>
+        </div>
+        )
+       
+    }
+    
+
+}
+
+export default Result
